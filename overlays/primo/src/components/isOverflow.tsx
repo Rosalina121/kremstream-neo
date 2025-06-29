948c7963da52bdbb0d39a843aa6e2be283bf2e31
Alina Rosa <rosalina@palitechnika.com>
2025-06-30 01:01:56 +0200

    add sauce

--- overlays/primo/src/components/isOverflow.tsx
+++ overlays/primo/src/components/isOverflow.tsx
@@ -1,23 +1,27 @@
+/*
+ * Based on https://www.robinwieruch.de/react-custom-hook-check-if-overflow/
+ */
+
 import * as React from 'react';
 
 export const useIsOverflow = (ref: any, callback: any) => {
-    const [isOverflow, setIsOverflow] = React.useState(false);
+  const [isOverflow, setIsOverflow] = React.useState(false);
 
-    React.useLayoutEffect(() => {
-        const { current } = ref;
+  React.useLayoutEffect(() => {
+    const { current } = ref;
 
-        const trigger = () => {
-            const hasOverflow = current.scrollHeight > current.clientHeight;
+    const trigger = () => {
+      const hasOverflow = current.scrollHeight > current.clientHeight;
 
-            setIsOverflow(hasOverflow);
+      setIsOverflow(hasOverflow);
 
-            if (callback) callback(hasOverflow);
-        };
+      if (callback) callback(hasOverflow);
+    };
 
-        if (current) {
-            trigger();
-        }
-    }, [callback, ref]);
+    if (current) {
+      trigger();
+    }
+  }, [callback, ref]);
 
-    return isOverflow;
+  return isOverflow;
 };
